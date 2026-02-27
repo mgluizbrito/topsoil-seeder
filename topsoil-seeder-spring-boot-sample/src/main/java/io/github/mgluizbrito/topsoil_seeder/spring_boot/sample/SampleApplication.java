06171857aa4bd1512430629e2d5ff7829d4cef4c
Luiz Miguel Brito <mgluiz1781@gmail.com>
2026-02-27 00:29:02 -0300

    feat: creating sample module with spring boot

---
+++ topsoil-seeder-spring-boot-sample/src/main/java/io/github/mgluizbrito/topsoil_seeder/spring_boot/sample/SampleApplication.java
@@ -0,0 +1,12 @@
+package io.github.mgluizbrito.topsoil_seeder.spring_boot.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
